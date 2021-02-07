c2d43fe5c401400ae3928ba0aedbd2bc4e7cd9d6
Beroozam <bmb.mm7@gmail.com>
2021-02-07 23:10:50 +0330

    lecture8-19

--- src/component/Burger/Burger.js
+++ src/component/Burger/Burger.js
@@ -6,14 +6,12 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map(( _ , i ) => {
-                console.log(igKey)
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             })
         })
         .reduce( (arr,el) => {
             return arr.concat(el)
         },[])
-        console.log(transformedIngredients)
     if(transformedIngredients.length === 0){
         transformedIngredients = <p>Please start adding ingredients!</p>;
     }
