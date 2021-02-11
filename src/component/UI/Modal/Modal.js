9a3aeeb945c25137aeffc4416f541a259228b43f
Beroozam <bmb.mm7@gmail.com>
2021-02-12 00:08:23 +0330

    lecture8-39

--- src/component/UI/Modal/Modal.js
+++ src/component/UI/Modal/Modal.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.module.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <React.Fragment>
-        <Backdrop 
-            show={props.show}
-            clicked={props.modalClosed}
-        />
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0',
-                visibility: props.show ? 'visible' : 'hidden'
-            }}
-            >
-            {props.children}
-        </div>
-    </React.Fragment>
-)
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps,nextState){
+        return nextProps.show !== this.props.show
+    }
+
+    componentWillUpdate(){
+        console.log('[Modal] WillUpdate')
+    }
+    
+    render(){
+        return(
+            <React.Fragment>
+                <Backdrop 
+                    show={this.props.show}
+                    clicked={this.props.modalClosed}
+                />
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0',
+                        visibility: this.props.show ? 'visible' : 'hidden'
+                    }}
+                    >
+                    {this.props.children}
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Modal;
